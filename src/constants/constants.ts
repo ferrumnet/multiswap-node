ca0d6eb6200a276b7d07aeb9cd411ca6ccdbccf2
Zikriya <zikriya.mushtaq@ferrum.network>
2023-03-09 15:08:26 +0500

    ethereumjs-util added to create the signature. Also contractAddress param have changed to static, only for BSC

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,2 +1,3 @@
-export const NAME = 'FiberRouter';
-export const VERSION = '000.001';
+export const NAME = 'FERRUM_TOKEN_BRIDGE_POOL';
+export const VERSION = '000.004';
+export const CONTRACT_ADDRESS = "0x9aFe354fb34a6303a9b9C89fF43A509A5320ba2D";
