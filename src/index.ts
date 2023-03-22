e66d5bc27ee1619a0565e0dcc74bcc0d63609259
Zikriya <zikriya.mushtaq@ferrum.network>
2023-03-23 00:22:04 +0500

    signature for foundar asset

--- src/index.ts
+++ src/index.ts
@@ -6,30 +6,3 @@ dotenv.config();
 const server = app.listen(process.env.PORT, () => {
   console.info(`Listening to port ${process.env.PORT}`);
 });
-
-const exitHandler = () => {
-  if (server) {
-    server.close(() => {
-      console.info('Server closed');
-      process.exit(1);
-    });
-  } else {
-    process.exit(1);
-  }
-};
-
-const unexpectedErrorHandler = (error: Error) => {
-  console.error(error);
-  exitHandler();
-};
-
-process.on('uncaughtException', unexpectedErrorHandler);
-process.on('unhandledRejection', unexpectedErrorHandler);
-
-process.on('SIGTERM', () => {
-  console.info('SIGTERM received');
-  if (server) {
-    server.close();
-    console.error('server closed');
-  }
-});
