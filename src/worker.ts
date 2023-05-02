6df57d10591a4b1352e9fa7c7b389902093c67e3
Zikriya <zikriya.mushtaq@ferrum.network>
2023-05-02 15:43:10 +0500

    cosmwasm lib added and also logic changed for evm and non-evm support inside worker

--- src/worker.ts
+++ src/worker.ts
@@ -1,10 +1,23 @@
 import { Worker } from 'bullmq';
-import { web3Service, axiosService } from './services';
+import { web3Service, axiosService, cosmWasmService } from './services';
 
 const worker = new Worker(
   process.env.QUEUE as string,
-  async job =>
-    await web3Service.getTransactionReceipt(job.data.txId, job.data.sourceRpcURL),
+  async job => {
+    if (job.data.isSourceNonEVM != null && job.data.isSourceNonEVM) {
+      console.log('isSourceNonEVM', true);
+      await cosmWasmService.getTransactionReceipt(
+        job.data.txId,
+        job.data.sourceRpcURL,
+      );
+    } else {
+      console.log('isSourceNonEVM', false);
+      await web3Service.getTransactionReceipt(
+        job.data.txId,
+        job.data.sourceRpcURL,
+      );
+    }
+  },
   {
     connection: {
       host: process.env.REDIS_HOST as string,
@@ -15,22 +28,29 @@ const worker = new Worker(
 worker.on('completed', async job => {
   try {
     console.info(`${job.id} has completed!`);
-    if(job && !job.returnvalue){
-      console.info(`Get latest receipt`);
-      job.returnvalue = await web3Service.getTransactionReceipt(job.data.txId, job.data.sourceRpcURL);
-    }
-    const decodedData = web3Service.getLogsFromTransactionReceipt(job);
-    const tx = await web3Service.getTransactionByHash(
-      job.data.txId,
-      job.data.sourceRpcURL,
-    );
-    const signedData = await web3Service.signedTransaction(job, decodedData, tx);
-    console.log('signedData', signedData);
-    axiosService.updateTransactionJobStatus(tx.hash, {
-      signedData,
-      transaction: tx,
-      transactionReceipt: job?.returnvalue,
-    });
+    // if (job && !job.returnvalue) {
+    //   console.info(`Get latest receipt`);
+    //   job.returnvalue = await web3Service.getTransactionReceipt(
+    //     job.data.txId,
+    //     job.data.sourceRpcURL,
+    //   );
+    // }
+    // const decodedData = web3Service.getLogsFromTransactionReceipt(job);
+    // const tx = await web3Service.getTransactionByHash(
+    //   job.data.txId,
+    //   job.data.sourceRpcURL,
+    // );
+    // const signedData = await web3Service.signedTransaction(
+    //   job,
+    //   decodedData,
+    //   tx,
+    // );
+    // console.log('signedData', signedData);
+    // axiosService.updateTransactionJobStatus(tx.hash, {
+    //   signedData,
+    //   transaction: tx,
+    //   transactionReceipt: job?.returnvalue,
+    // });
   } catch (error) {
     console.error('error occured', error);
   }
