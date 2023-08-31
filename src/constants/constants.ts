f95246a03c90d8fe1a215746ded23e5a53bbff7c
Zikriya <zikriya.mushtaq@ferrum.network>
2023-09-01 02:34:04 +0500

    contract name changed

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,4 @@
-export const NAME = 'FUND MANAGER';
+export const NAME = 'FERRUM_TOKEN_BRIDGE_POOL';
 export const VERSION = '000.004';
 export const CONTRACT_ADDRESS = '0x9aFe354fb34a6303a9b9C89fF43A509A5320ba2D';
 export const BEARER = 'Bearer ';
